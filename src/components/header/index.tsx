be4334dce03ec8066f61d9600130d22529b2c133
David <electromandarin@gmail.com>
2023-01-21 21:48:02 +0200

    feat: google auth

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,69 @@
-import { signInWithPopup } from "firebase/auth";
+import { onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { auth, provider } from "../../service/firebase";
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
+
+interface UserObj {
+  displayName: string;
+  avatarURL: string;
+}
 
 const Header = () => {
+  const [user, setUser] = useState<UserObj | null>(null);
+
+  const signIn = () => {
+    signInWithPopup(auth, provider).then((data: any) =>
+      setUser({
+        displayName: data.user.displayName,
+        avatarURL: data.url.photoURL,
+      })
+    );
+  };
+
+  useEffect(() => {
+    const observer = onAuthStateChanged(auth, (user) => {
+      console.log(user)
+      if (user) {
+        setUser({
+          displayName: user.displayName!,
+          avatarURL: user.photoURL!,
+        });
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => observer();
+  }, []);
+  // console.log(user)
   return (
     <div className={styles.header}>
-      <button onClick={() => signInWithPopup(auth, provider)} className={styles.loginBtn}>
-        <span className={styles.loginBtnText}>Login</span>
-        <img src="/images/ic_google.svg" alt="Google icon" className={styles.loginBtnLogo} />
+      {user && <div className={styles.user}>
+          <img className={styles.userAvatar} src={user.avatarURL} referrerPolicy="no-referrer" alt="" />
+          <span className={styles.userName}>{user.displayName}</span>
+        </div>}
+      <button
+        onClick={user ? () => signOut(auth) : signIn}
+        className={styles.loginBtn}
+      >
+        {!user ? (
+          <>
+            <span className={styles.loginBtnText}>Login</span>
+            <img
+              src="/images/ic_google.svg"
+              alt="Google icon"
+              className={styles.loginBtnLogo}
+            />
+          </>
+        ) : (
+          <>
+            <img
+              src="/images/ic_logout.svg"
+              alt=""
+              className={styles.loginBtnLogo}
+            />
+          </>
+        )}
       </button>
     </div>
   );
